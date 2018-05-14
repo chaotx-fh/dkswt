b58b812294e66a38d93d6f2645197d796c15ce52
chaotx-eu <chaotx@zoho.eu>
2018-05-14 23:06:18 +0200

    reorganized packages

--- java/src/eu/zoho/chaotx/doppelkopf/server/App.java
+++ java/src/eu/zoho/chaotx/doppelkopf/server/App.java
@@ -1,8 +1,7 @@
-package eu.zoho.chaotx.server;
+package eu.zoho.chaotx.doppelkopf.server;
 
 class App {
     public static void main(String[] args) {
         System.out.println("yoyoyoyoyo");
     }
-
 }
