ec9e0351e7d93d589d9bec3b3281a36675fb987a
hdainester <snahreteid@gmx.de>
2018-05-15 00:35:17 +0200

    test

--- java/src/eu/zoho/chaotx/doppelkopf/server/App.java
+++ java/src/eu/zoho/chaotx/doppelkopf/server/App.java
@@ -2,6 +2,6 @@ package eu.zoho.chaotx.doppelkopf.server;
 
 class App {
     public static void main(String[] args) {
-        System.out.println("yoyoyoyoyo");
+        System.out.println("eyeyeyeyeyey");
     }
 }
