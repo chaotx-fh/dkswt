10b542379556388294f43775a11b92c44dc634a8
hdainester <chaotx@zoho.eu>
2018-05-15 14:03:07 +0200

    test commit

--- java/src/eu/zoho/chaotx/doppelkopf/server/App.java
+++ java/src/eu/zoho/chaotx/doppelkopf/server/App.java
@@ -3,5 +3,6 @@ package eu.zoho.chaotx.doppelkopf.server;
 class App {
     public static void main(String[] args) {
         System.out.println("eyeyeyeyeyey");
+        System.out.println("yesyesyes");
     }
 }
