b45a20f7ec1da44b5027706c0b299176d18c088e
hdainester <chaotx@zoho.eu>
2018-05-15 18:08:47 +0200

    Zwischenstand - Game: run method - DKSession: start method - SessionManager: runSessions method

--- java/src/eu/zoho/chaotx/doppelkopf/game/Game.java
+++ java/src/eu/zoho/chaotx/doppelkopf/game/Game.java
@@ -1,5 +1,10 @@
 package eu.zoho.chaotx.doppelkopf.game;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.oracle.jrockit.jfr.InvalidValueException;
+
 public class Game implements Runnable {
     private Board board;
     private Player nextplayer;
@@ -8,5 +13,11 @@ public class Game implements Runnable {
 
     public Game(Player[] participants, Board board) {
         // something like that
+    }
+
+    @Override
+    public void run() {
+        if(Player.length == 0)
+            return;
     }
 }
--- java/src/eu/zoho/chaotx/doppelkopf/server/App.java
+++ java/src/eu/zoho/chaotx/doppelkopf/server/App.java
@@ -1,5 +1,7 @@
 package eu.zoho.chaotx.doppelkopf.server;
 
+import eu.zoho.chaotx.doppelkopf.game.Game;
+
 class App {
     public static void main(String[] args) {
         System.out.println("spiel startet");
