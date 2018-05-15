423b53827baa7f68c8c919aaafcbdca13bc2e17b
Safouane Knani <saf.knani@gmail.com>
2018-05-15 18:11:25 +0200

    test

--- java/src/eu/zoho/chaotx/doppelkopf/server/App.java
+++ java/src/eu/zoho/chaotx/doppelkopf/server/App.java
@@ -2,11 +2,11 @@ package eu.zoho.chaotx.doppelkopf.server;
 
 class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("spiel startet");
-=======
-        System.out.println("eyeyeyeyeyey");
+
+        System.out.println("gi");
         System.out.println("yesyesyes");
->>>>>>> 10b542379556388294f43775a11b92c44dc634a8
+
     }
 }
