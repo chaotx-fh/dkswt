c5854827f8255230866f8767e2df5df14d294006
Kekz69 <Manuel_92@web.de>
2018-05-16 18:00:00 +0200

    Enum

--- java/src/eu/zoho/chaotx/doppelkopf/game/Card.java
+++ java/src/eu/zoho/chaotx/doppelkopf/game/Card.java
@@ -2,7 +2,12 @@ package eu.zoho.chaotx.doppelkopf.game;
 
 // TODO
 public class Card {
+    
     private int value;
+
+    public enum Symbol{
+        HEART, DIAMOND, SPADE,CLUB
+    }//HEART = Herz, DIAMOND = Karo, SPADE = Pik ,CLUB = Kreuz
     
     public Card() {
 
